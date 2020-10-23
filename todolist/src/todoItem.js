b0eec8dc77a2bcc75b11475c0a68af228b82c745
a23246542 <a23246542@gmail.com>
2020-10-23 11:29:06 +0800

    todo優化

--- todolist/src/todoItem.js
+++ todolist/src/todoItem.js
@@ -8,18 +8,20 @@ class TodoItem extends React.Component {
   }
 
   render() {
+    const {content} = this.props; 
     return (
       // index.js:1 Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.
       // @@这些为舌么不能直接写上方法 //数据可以直接传 方法除了父组件要绑定this 子组件还要放入另外的函式?
       // <li onClick={this.props.delItem(this.props.index)}>
       <li onClick={this.handleClick}>
-        {this.props.content}
+        {content}
       </li>
     )
   }
 
   handleClick() {
-    this.props.delItem(this.props.index);
+    const {delItem, index}  = this.props;
+    delItem(index);
     // 等于
     // this.props.handleDelItem(this.props.index);
   }
--- todolist/src/todolist.js
+++ todolist/src/todolist.js
@@ -10,6 +10,11 @@ class TodoList extends React.Component {
       list: ['學習英文','學習react'],
       //接下來綁定好了 要怎麼實現新增刪除功能 別館dom了 只要想怎麼把inputValue放進list就行了
     }
+    this.getTodoItems = this.getTodoItems.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    // this.handleDelItem = this.handleDelItem.bind(this); //!!名词改用后缀
+    this.handleItemDel = this.handleItemDel.bind(this);
   }
   render() {
     return (
@@ -22,17 +27,17 @@ class TodoList extends React.Component {
           }
           <input
             type="text" value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
           >
           </input>
           <button
-            onClick={this.handleBtnClick.bind(this)}
+            onClick={this.handleBtnClick}
           >
             提交
           </button>
         </div>
         <ul>
-          {
+          {/* {
             this.state.list.map((item, index) =>{
               // return <li key={index}>{item}</li>
               return ( //@@老師說代碼做個格式化 加上()差在哪?
@@ -41,20 +46,36 @@ class TodoList extends React.Component {
                 //   dangerouslySetInnerHTML = {{__html:item}} //%%兩個底線
                 //   onClick = { this.handleDelItem.bind(this, index) }
                 // >
-                //     {/* {item} */}
                 // </li>
                 <TodoItem 
                   key={index}
                   content={item}
                   index={index}
-                  delItem = {this.handleDelItem.bind(this)}
+                  delItem = {this.handleDelItem}
                 ></TodoItem>
               )
             })
-          }
+          } */}
+
+          {/*%%事件绑定才不会刮号 回传数据要刮号 index.js:1 Warning: Functions are not valid as a React child. This may happen if you return a Component instead of <Component /> from render. Or maybe you meant to call this function rather than return it. */}
+          {/* {this.getTodoItems } */}
+
+          {this.getTodoItems()}
         </ul>
       </Fragment>
     )
+  }
+  getTodoItems() { //@@ 其实不太懂 直接写 不是function XX才可以吗
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={item}
+          index={index}
+          delItem = {this.handleItemDel}
+        />
+      )
+    })
   }
 
   handleInputChange(e) {
@@ -65,24 +86,50 @@ class TodoList extends React.Component {
     // this是undefined  改用bind可以綁定這個函式的this
     // 第二坑 沒用setState
     // this.state.inputValue  = e.target.value; //
-    this.setState({
-      inputValue: e.target.value,
-    })
+
+    // this.setState({
+    //   inputValue: e.target.value,
+    // })
+
+    const value = e.target.value;//%%
+    this.setState((e)=>({ //!刮号是要回传的东西 代替{return 物件} %%忘记写e 虽然没报错
+        inputValue:value
+    }))
   }
 
   handleBtnClick() {
-    this.setState({
-      list:[...this.state.list,this.state.inputValue],
-      inputValue:''
+    // this.setState({
+    //   list:[...this.state.list,this.state.inputValue],
+    //   inputValue:''
+    // })
+
+    this.setState((prevState)=>{ //!可对比上面纯刮号
+      // return({
+      //   list:[...prevState.list,prevState.inputValue],
+      //   inputValue:'',
+      // })
+      return { //@@所以直接返回对象就好
+        list:[...prevState.list,prevState.inputValue],
+        inputValue:'',
+      }
     })
   }
 
-  handleDelItem(index) {
-    const list = [...this.state.list];
-    list.splice(index,1);
-    this.setState({
-      list:list,
-    })
+  handleItemDel(index) {
+    // const list = [...this.state.list];
+    // list.splice(index,1);
+    // this.setState({
+    //   list:list,
+    // })
+
+    this.setState((prevState) => {
+      const list = [...prevState.list,prevState.inputValue];
+      list.splice(index,1);
+      // return({ // @@好像也可以
+      //   list //es6物件缩写
+      // })
+      return{list}
+    }) 
   }
 
   // immutable的概念 不允許state做任何的改變 拷貝一個副本修改做替換
